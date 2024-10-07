e59c61ab50abb44c234f4ef08d21c2311bf69a5c
Liam <liammtech@gmail.com>
2024-10-07 19:22:23 +0100

    Add string replacement example

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+/* From original TOP lesson */
 function favouriteAnimal(animal) {
     return animal + " is your favourite animal!"
 }
 
 alert(favouriteAnimal(prompt("What is your favourite animal?")))
+
+/* String manipulation */
+
+const myText = "This is an example string";
+const myNewText = myText.replace("example", "exemplary");
+console.log(myNewText);
